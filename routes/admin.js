a4a90ddb4e990d1d90589a756346758d1569afaa
Muktadir Imam Jan <muktadirimam@gmail.com>
2019-02-28 01:06:58 +0600

    admin routes get-post updated

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const productController = require("../controller/product");
+const adminController = require("../controller/admin");
 
 /* GET home page. */
-router.get("/add-product", productController.getAddProduct);
+router.get("/add-product", adminController.getAddProduct);
 
-router.post("/add-product", productController.postAddProduct);
+router.get("/products", adminController.getProducts);
+
+router.get("/products", adminController.getProducts);
+
+router.post("/add-product", adminController.postAddProduct);
+
+router.get("/edit-product/:productId", adminController.getEditProduct);
+
+router.post("/edit-product", adminController.postEditProduct);
+
+router.post("/delete-product", adminController.postDeleteProduct);
 
 module.exports = router;
