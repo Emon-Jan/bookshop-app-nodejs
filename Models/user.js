d4a0cc1f1f0c7d4332001c0b05f6e40346599984
Muktadir Imam Jan <muktadirimam@gmail.com>
2019-03-02 03:15:12 +0600

    user cart functionality added

--- Models/user.js
+++ Models/user.js
@@ -28,4 +28,39 @@ const userSchema = new Schema({
 	},
 });
 
+userSchema.methods.addToCart = function(product) {
+	const cartProductIndex = this.cart.items.findIndex(cp => {
+		return cp.productId.toString() === product._id.toString();
+	});
+
+	let newItemQty = 1;
+	const updatedCartItems = [...this.cart.items];
+	if (cartProductIndex >= 0) {
+		newItemQty = this.cart.items[cartProductIndex].quantity + 1;
+		updatedCartItems[cartProductIndex].quantity = newItemQty;
+	} else {
+		updatedCartItems.push({
+			productId: product._id,
+			quantity: newItemQty,
+		});
+	}
+	const updatedCart = {
+		items: updatedCartItems,
+	};
+	this.cart = updatedCart;
+	return this.save();
+};
+
+userSchema.methods.removeFromCart = function(productId) {
+	const updatedCartItems = this.cart.items.filter(item => {
+		return item.productId.toString() !== productId.toString();
+	});
+	this.cart.items = updatedCartItems;
+	return this.save();
+};
+
+userSchema.methods.clearCart = function() {
+	this.cart.items = [];
+	return this.save();
+};
 module.exports = mongoose.model("User", userSchema);
